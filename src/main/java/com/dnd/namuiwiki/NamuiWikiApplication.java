2663d1c1d8df089384e7adf39b558439243c100e
김찬현 <chanhyun@pavv.local>
2024-01-27 23:22:45 +0900

    initial setting

---
+++ src/main/java/com/dnd/namuiwiki/NamuiWikiApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.namuiwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamuiWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamuiWikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnd/namuiwiki/NamuiWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.namuiwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamuiWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
