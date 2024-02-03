521159789c886f9a1bc22d68119e2ffe8afa0d51
eun-seong <eunseong.dev@gmail.com>
2024-02-03 16:14:29 +0900

    refactor: OAuth 핸들러에 인스턴스 등록 후 찾아 사용하는 방식으로 변경(#1)

--- src/main/java/com/dnd/namuiwiki/application/oauth/OAuthService.java
+++ src/main/java/com/dnd/namuiwiki/application/oauth/OAuthService.java
@@ -5,21 +5,25 @@ import com.dnd.namuiwiki.application.oauth.kakao.OAuthKakaoService;
 import com.dnd.namuiwiki.application.oauth.type.OAuthProvider;
 import com.dnd.namuiwiki.common.exception.ApplicationErrorException;
 import com.dnd.namuiwiki.common.exception.ApplicationErrorType;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
-@RequiredArgsConstructor
 public class OAuthService {
-    private final OAuthKakaoService oAuthKakaoService;
+    private final Map<OAuthProvider, OAuthProviderService> providerServiceHandler = new HashMap<>();
+
+    public OAuthService(OAuthKakaoService oAuthKakaoService) {
+        providerServiceHandler.put(OAuthProvider.KAKAO, oAuthKakaoService);
+    }
 
     public OAuthUserInfoDto oauthLogin(String provider, String code) {
-        switch (OAuthProvider.of(provider)) {
-            case KAKAO:
-                return oAuthKakaoService.getOAuthUserInfo(code);
-            default:
-                throw new ApplicationErrorException(ApplicationErrorType.INVALID_DATA_ARGUMENT, "지원하지 않는 프로바이더입니다.");
+        OAuthProviderService providerService = providerServiceHandler.get(OAuthProvider.of(provider));
+        if (providerService == null) {
+            throw new ApplicationErrorException(ApplicationErrorType.INVALID_DATA_ARGUMENT, "지원하지 않는 프로바이더입니다.");
         }
+        return providerService.getOAuthUserInfo(code);
     }
 
 }
