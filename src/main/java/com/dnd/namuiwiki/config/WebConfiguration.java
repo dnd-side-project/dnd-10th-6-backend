ac7f66ea8b9de0aad2fedd5a39f43a155a11142d
eun-seong <eunseong.dev@gmail.com>
2024-02-07 19:58:25 +0900

    feat: WebMvcConfiguration에 cors 관련 설정 추가(#7)

---
+++ src/main/java/com/dnd/namuiwiki/config/WebConfiguration.java
@@ -0,0 +1,24 @@
+package com.dnd.namuiwiki.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Value("${spring.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
+    @Value("${spring.cors.allowed-methods}")
+    private String[] allowedMethods;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods(allowedMethods)
+                .allowCredentials(true);
+    }
+}
