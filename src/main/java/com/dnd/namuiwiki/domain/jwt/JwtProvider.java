c55172d2ba686629c2ef31d50e324c9c1a8af47b
rlacksgus97 <rlacksgus97@naver.com>
2024-02-09 19:10:54 +0900

    fix: JwtProvider가 @PostConstruct를 사용하도록 수정(#13)

--- src/main/java/com/dnd/namuiwiki/domain/jwt/JwtProvider.java
+++ src/main/java/com/dnd/namuiwiki/domain/jwt/JwtProvider.java
@@ -6,6 +6,7 @@ import com.dnd.namuiwiki.domain.jwt.dto.TokenUserInfoDto;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -14,7 +15,7 @@ import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component
-public class JwtProvider implements InitializingBean {
+public class JwtProvider {
 
     @Value("${jwt.key}")
     public String key;
@@ -27,7 +28,7 @@ public class JwtProvider implements InitializingBean {
     private final String WIKI_ID = "WIKI_ID";
     private SecretKey secretKey;
 
-    @Override
+    @PostConstruct
     public void afterPropertiesSet() {
         byte[] keyBytes = Decoders.BASE64.decode(key);
         this.secretKey = Keys.hmacShaKeyFor(keyBytes);
