65f9b8c1260f90fd5599ac94be62be0d6aab58d0
eun-seong <eunseong.dev@gmail.com>
2024-02-11 18:11:39 +0900

    test: 문항 유형별 옵션 유형 정의(#11)

--- src/main/java/com/dnd/namuiwiki/domain/question/type/QuestionType.java
+++ src/main/java/com/dnd/namuiwiki/domain/question/type/QuestionType.java
@@ -1,13 +1,27 @@
 package com.dnd.namuiwiki.domain.question.type;
 
-public enum QuestionType {
-    OX, MULTIPLE_CHOICE, SHORT_ANSWER, NUMBER_CHOICE;
+import com.dnd.namuiwiki.domain.survey.type.AnswerType;
 
-    public boolean isChoice() {
+import java.util.List;
+
+public enum QuestionType {
+    OX(List.of(AnswerType.OPTION)),
+    MULTIPLE_CHOICE(List.of(AnswerType.MANUAL, AnswerType.OPTION)),
+    SHORT_ANSWER(List.of(AnswerType.MANUAL)),
+    NUMBER_CHOICE(List.of(AnswerType.MANUAL, AnswerType.OPTION));
+
+    QuestionType(List<AnswerType> answerTypes) {
+        this.answerTypes = answerTypes;
+    }
+
+    private final List<AnswerType> answerTypes;
+
+    public boolean containsAnswerType(AnswerType answerType) {
+        return this.answerTypes.contains(answerType);
+    }
+
+    public boolean isChoiceType() {
         return this == MULTIPLE_CHOICE || this == OX || this == NUMBER_CHOICE;
     }
 
-    public static QuestionType of(String type) {
-        return QuestionType.valueOf(type);
-    }
 }
