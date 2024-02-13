b35e0cc07180353718db4e13ccdf09b5a736ca42
eun-seong <eunseong.dev@gmail.com>
2024-02-14 03:03:56 +0900

    fix: repository가 User 가져오지 못하던 에러 수정

--- src/main/java/com/dnd/namuiwiki/domain/user/UserService.java
+++ src/main/java/com/dnd/namuiwiki/domain/user/UserService.java
@@ -1,12 +1,14 @@
 package com.dnd.namuiwiki.domain.user;
 
-import com.dnd.namuiwiki.domain.oauth.dto.OAuthUserInfoDto;
-import com.dnd.namuiwiki.domain.jwt.JwtService;
 import com.dnd.namuiwiki.domain.auth.dto.OAuthLoginResponse;
+import com.dnd.namuiwiki.domain.jwt.JwtService;
+import com.dnd.namuiwiki.domain.oauth.dto.OAuthUserInfoDto;
 import com.dnd.namuiwiki.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -18,10 +20,11 @@ public class UserService {
     @Transactional
     public OAuthLoginResponse login(OAuthUserInfoDto oAuthUserInfoDto) {
         User user = userRepository.findByOauthProviderAndOauthId(oAuthUserInfoDto.getProvider(), oAuthUserInfoDto.getOAuthId())
-                .orElseGet(() -> {
-                    User newUser = new User(oAuthUserInfoDto.getProvider(), oAuthUserInfoDto.getOAuthId());
-                    return userRepository.save(newUser);
-                });
+                .orElseGet(() -> userRepository.save(User.builder()
+                        .wikiId(UUID.randomUUID().toString())
+                        .oauthProvider(oAuthUserInfoDto.getProvider())
+                        .oauthId(oAuthUserInfoDto.getOAuthId())
+                        .build()));
         OAuthLoginResponse oAuthLoginResponse = jwtService.issueTokenPair(user.getWikiId());
         user.setRefreshToken(oAuthLoginResponse.getRefreshToken());
         return oAuthLoginResponse;
