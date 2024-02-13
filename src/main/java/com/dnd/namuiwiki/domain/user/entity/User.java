df796af08343bb5132f230d7b076c597e1f6b29b
eun-seong <eunseong.dev@gmail.com>
2024-02-14 04:29:20 +0900

    feat: 나에게 설문을 보냈을 시 에러 발생(#11)

--- src/main/java/com/dnd/namuiwiki/domain/user/entity/User.java
+++ src/main/java/com/dnd/namuiwiki/domain/user/entity/User.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Objects;
+
 @Getter
 @Builder
 @Document(collection = "users")
@@ -19,5 +21,18 @@ public class User {
 
     public void setRefreshToken(String refreshToken) {
         this.refreshToken = refreshToken;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(wikiId, user.wikiId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(wikiId);
     }
 }
