81ffbad622998b151ce9ee0cc02668d26cad41a2
eun-seong <eunseong.dev@gmail.com>
2024-02-16 00:56:31 +0900

    feat: OAuth 로그인 시 회원가입 여부 판단하여 응답하도록 수정(#29)

--- src/main/java/com/dnd/namuiwiki/common/exception/ApplicationErrorException.java
+++ src/main/java/com/dnd/namuiwiki/common/exception/ApplicationErrorException.java
@@ -1,11 +1,14 @@
 package com.dnd.namuiwiki.common.exception;
 
 import lombok.Getter;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 @Getter
 public class ApplicationErrorException extends RuntimeException {
     private final ApplicationErrorType errorType;
     private String customMessage;
+    private MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
 
     public ApplicationErrorException(ApplicationErrorType errorType) {
         super(errorType.getMessage());
@@ -28,4 +31,31 @@ public class ApplicationErrorException extends RuntimeException {
         this.errorType = errorType;
         this.customMessage = customMessage;
     }
+
+    public ApplicationErrorException(ApplicationErrorType errorType, MultiValueMap<String, String> headers) {
+        super(errorType.getMessage());
+        this.errorType = errorType;
+        this.headers = headers;
+    }
+
+    public ApplicationErrorException(ApplicationErrorType errorType, String customMessage, MultiValueMap<String, String> headers) {
+        super(errorType.getMessage());
+        this.errorType = errorType;
+        this.customMessage = customMessage;
+        this.headers = headers;
+    }
+
+    public ApplicationErrorException(ApplicationErrorType errorType, Throwable t, MultiValueMap<String, String> headers) {
+        super(t);
+        this.errorType = errorType;
+        this.headers = headers;
+    }
+
+    public ApplicationErrorException(ApplicationErrorType errorType, Throwable t, String customMessage, MultiValueMap<String, String> headers) {
+        super(t);
+        this.errorType = errorType;
+        this.customMessage = customMessage;
+        this.headers = headers;
+    }
+
 }
