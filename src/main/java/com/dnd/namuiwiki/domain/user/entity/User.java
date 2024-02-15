5c3a26d64f787201acb675e8ec7dcbc79b6d7ccc
eun-seong <eunseong.dev@gmail.com>
2024-02-15 16:28:03 +0900

    fix: User에 기본 생성자 추가(#24)

--- src/main/java/com/dnd/namuiwiki/domain/user/entity/User.java
+++ src/main/java/com/dnd/namuiwiki/domain/user/entity/User.java
@@ -2,12 +2,14 @@ package com.dnd.namuiwiki.domain.user.entity;
 
 import com.dnd.namuiwiki.domain.oauth.type.OAuthProvider;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
 
 @Getter
+@NoArgsConstructor
 @Document(collection = "users")
 public class User {
     @Id
