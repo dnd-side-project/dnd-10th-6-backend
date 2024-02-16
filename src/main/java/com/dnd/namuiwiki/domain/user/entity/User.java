f778e99a7e345d7362cbd9cb321de438150bd82c
rlacksgus97 <rlacksgus97@naver.com>
2024-02-16 21:15:16 +0900

    fix: merge conflict

--- src/main/java/com/dnd/namuiwiki/domain/user/entity/User.java
+++ src/main/java/com/dnd/namuiwiki/domain/user/entity/User.java
@@ -1,13 +1,16 @@
 package com.dnd.namuiwiki.domain.user.entity;
 
 import com.dnd.namuiwiki.domain.oauth.type.OAuthProvider;
+import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Getter
+@Builder
 @Document(collection = "users")
 public class User {
     @Id
@@ -25,5 +28,18 @@ public class User {
 
     public void setRefreshToken(String refreshToken) {
         this.refreshToken = refreshToken;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(wikiId, user.wikiId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(wikiId);
     }
 }
