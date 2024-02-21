dd674c2037a5d35ba000dd1d23584780537bef64
eun-seong <eunseong.dev@gmail.com>
2024-02-22 04:52:21 +0900

    feat: Dashboard Controller 스웨거 추가(#52)

--- src/main/java/com/dnd/namuiwiki/domain/dashboard/DashboardController.java
+++ src/main/java/com/dnd/namuiwiki/domain/dashboard/DashboardController.java
@@ -1,10 +1,16 @@
 package com.dnd.namuiwiki.domain.dashboard;
 
 import com.dnd.namuiwiki.common.dto.ResponseDto;
+import com.dnd.namuiwiki.domain.dashboard.model.dto.DashboardDto;
 import com.dnd.namuiwiki.domain.jwt.JwtAuthorization;
 import com.dnd.namuiwiki.domain.jwt.dto.TokenUserInfoDto;
 import com.dnd.namuiwiki.domain.survey.type.Period;
 import com.dnd.namuiwiki.domain.survey.type.Relation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,19 +18,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "대시보드", description = "Dashboard API")
 @RestController
 @RequestMapping("/api/v1/dashboard")
 @RequiredArgsConstructor
 public class DashboardController {
     private final DashboardService dashboardService;
 
+    @Operation(summary = "대시 보드 조회", responses = {
+            @ApiResponse(responseCode = "200", description = "대시보드 조회 성공", content = @Content(schema = @Schema(implementation = DashboardDto.class))),
+    })
     @GetMapping
     public ResponseEntity<?> getDashboard(
             @JwtAuthorization TokenUserInfoDto tokenUserInfoDto,
             @RequestParam(required = false) Period period,
             @RequestParam(required = false) Relation relation
     ) {
-        var response = dashboardService.getDashboard(tokenUserInfoDto, period, relation);
+        DashboardDto response = dashboardService.getDashboard(tokenUserInfoDto, period, relation);
         return ResponseDto.ok(response);
     }
 
--- src/main/java/com/dnd/namuiwiki/domain/dashboard/model/dto/GetDashboardRequest.java
+++ src/main/java/com/dnd/namuiwiki/domain/dashboard/model/dto/GetDashboardRequest.java
@@ -3,8 +3,10 @@ package com.dnd.namuiwiki.domain.dashboard.model.dto;
 import com.dnd.namuiwiki.common.annotation.Enum;
 import com.dnd.namuiwiki.domain.survey.type.Period;
 import com.dnd.namuiwiki.domain.survey.type.Relation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
+@Schema(description = "대시보드 조회 요청 body")
 @Getter
 public class GetDashboardRequest {
 
