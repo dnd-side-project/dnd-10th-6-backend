4479d9e7e110989b4f58af60a41236bf8e75535d
eun-seong <eunseong.dev@gmail.com>
2024-02-22 05:11:31 +0900

    feat: Survey Controller 스웨거 추가(#52)

--- src/main/java/com/dnd/namuiwiki/domain/survey/model/dto/AnswerDto.java
+++ src/main/java/com/dnd/namuiwiki/domain/survey/model/dto/AnswerDto.java
@@ -2,10 +2,12 @@ package com.dnd.namuiwiki.domain.survey.model.dto;
 
 import com.dnd.namuiwiki.common.annotation.Enum;
 import com.dnd.namuiwiki.domain.survey.type.AnswerType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Getter;
 
+@Schema(description = "질문별 답변")
 @Getter
 @Builder
 public class AnswerDto {
