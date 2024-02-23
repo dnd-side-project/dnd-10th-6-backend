32fc495a32e810101abffcf05fef085c4708da82
eun-seong <eunseong.dev@gmail.com>
2024-02-24 03:13:00 +0900

    feat: 빌릴 수 있는 액수 범위 설정(#75)

--- src/main/java/com/dnd/namuiwiki/common/exception/ApplicationErrorType.java
+++ src/main/java/com/dnd/namuiwiki/common/exception/ApplicationErrorType.java
@@ -62,6 +62,8 @@ public enum ApplicationErrorType {
     NOT_FOUND_SURVEY(HttpStatus.NOT_FOUND, "존재하지 않는 설문입니다."),
     QUESTION_ANSWER_COUNT_NOT_EQUAL(HttpStatus.INTERNAL_SERVER_ERROR, "문항과 답변의 개수가 일치하지 않습니다."),
 
+    INVALID_BORROWING_LIMIT(HttpStatus.BAD_REQUEST, "빌릴 수 있는 돈은 0 이상 10억 이하입니다."),
+
     /**
      * Filter Error Type
      */
