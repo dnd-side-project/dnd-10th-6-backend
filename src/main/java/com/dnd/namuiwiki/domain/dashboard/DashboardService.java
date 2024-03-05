a8e47b6efc39c2ce25e9de6ac12aeebbb0e9fd59
eun-seong <eunseong.dev@gmail.com>
2024-03-05 18:31:22 +0900

    refactor: 유저통계량과 전체통계량 계산 로직 분리(#104)

--- src/main/java/com/dnd/namuiwiki/domain/dashboard/DashboardService.java
+++ src/main/java/com/dnd/namuiwiki/domain/dashboard/DashboardService.java
@@ -18,6 +18,7 @@ import com.dnd.namuiwiki.domain.statistic.StatisticsService;
 import com.dnd.namuiwiki.domain.statistic.model.BorrowingLimitEntireStatistic;
 import com.dnd.namuiwiki.domain.dashboard.model.Statistics;
 import com.dnd.namuiwiki.domain.statistic.model.entity.PopulationStatistic;
+import com.dnd.namuiwiki.domain.survey.model.entity.Survey;
 import com.dnd.namuiwiki.domain.survey.type.Period;
 import com.dnd.namuiwiki.domain.survey.type.Relation;
 import com.dnd.namuiwiki.domain.user.UserRepository;
@@ -81,6 +82,40 @@ public class DashboardService {
     private User findByWikiId(String wikiId) {
         return userRepository.findByWikiId(wikiId)
                 .orElseThrow(() -> new ApplicationErrorException(ApplicationErrorType.NOT_FOUND_USER));
+    }
+
+
+    public void updateStatistics(Survey survey) {
+        User owner = survey.getOwner();
+        Period period = survey.getPeriod();
+        Relation relation = survey.getRelation();
+
+        var statisticalAnswers = survey.getAnswers().stream()
+                .filter(answer -> answer.getQuestion().getDashboardType().getStatisticsType().isNotNone())
+                .toList();
+
+        updateDashboards(owner, period, relation, statisticalAnswers);
+    }
+
+    private void updateDashboards(User owner, Period period, Relation relation, List<Survey.Answer> statisticalAnswers) {
+        updateDashboardByCategory(owner, null, null, statisticalAnswers);
+        updateDashboardByCategory(owner, period, null, statisticalAnswers);
+        updateDashboardByCategory(owner, null, relation, statisticalAnswers);
+    }
+
+    private void updateDashboardByCategory(User owner, Period period, Relation relation, List<Survey.Answer> answers) {
+        Dashboard dashboard = dashboardRepository.findByUserAndPeriodAndRelation(owner, period, relation)
+                .orElseGet(() -> {
+                    Dashboard newDashboard = Dashboard.builder()
+                            .user(owner)
+                            .period(period)
+                            .relation(relation)
+                            .statistics(Statistics.from(answers.stream().map(Survey.Answer::getQuestion).toList()))
+                            .build();
+                    return dashboardRepository.save(newDashboard);
+                });
+        dashboard.updateStatistics(answers);
+        dashboardRepository.save(dashboard);
     }
 
 }
