cf57429bba2609d0eb73c909006f9139166ef177
eun-seong <eunseong.dev@gmail.com>
2024-03-14 00:36:28 +0900

    feat: 비동기 메소드 예외 처리 핸들러 추가(#92)

---
+++ src/main/java/com/dnd/namuiwiki/common/exception/AsyncExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.dnd.namuiwiki.common.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+public class AsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
+
+    @Override
+    public void handleUncaughtException(Throwable ex, Method method, Object... params) {
+        log.error("AsyncException message={}, declaringClass={}, methodName={}", ex.getMessage(), method.getDeclaringClass(), method.getName(), ex);
+
+        for (Object param : params) {
+            log.error("Parameter value - {}, declaringClass={}, methodName={}", param, method.getDeclaringClass(), method.getName());
+        }
+
+    }
+
+}
--- src/main/java/com/dnd/namuiwiki/config/AsyncConfiguration.java
+++ src/main/java/com/dnd/namuiwiki/config/AsyncConfiguration.java
@@ -1,9 +1,24 @@
 package com.dnd.namuiwiki.config;
 
+import com.dnd.namuiwiki.common.exception.AsyncExceptionHandler;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @Configuration
-public class AsyncConfiguration {
+public class AsyncConfiguration implements AsyncConfigurer {
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return asyncExceptionHandler();
+    }
+
+    @Bean
+    public AsyncExceptionHandler asyncExceptionHandler() {
+        return new AsyncExceptionHandler();
+    }
+
 }
