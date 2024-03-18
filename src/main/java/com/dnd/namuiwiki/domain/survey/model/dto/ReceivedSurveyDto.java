e9de06852103335032d8bc722b74af3f4a793e45
rlacksgus97 <rlacksgus97@naver.com>
2024-03-18 22:05:01 +0900

    fix: 내가 받은 설문에 작성자 wikiId 포함(#126)

--- src/main/java/com/dnd/namuiwiki/domain/survey/model/dto/ReceivedSurveyDto.java
+++ src/main/java/com/dnd/namuiwiki/domain/survey/model/dto/ReceivedSurveyDto.java
@@ -3,8 +3,11 @@ package com.dnd.namuiwiki.domain.survey.model.dto;
 import com.dnd.namuiwiki.domain.survey.model.entity.Survey;
 import com.dnd.namuiwiki.domain.survey.type.Period;
 import com.dnd.namuiwiki.domain.survey.type.Relation;
+import com.dnd.namuiwiki.domain.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.Optional;
 
 @Getter
 @AllArgsConstructor
@@ -14,13 +17,21 @@ public class ReceivedSurveyDto {
     private Relation relation;
     private Period period;
     private String senderName;
+    private String senderWikiId;
 
     public static ReceivedSurveyDto from(Survey survey) {
+        String senderWikiId = null;
+        User sender = survey.getSender();
+        if (sender != null) {
+            senderWikiId = sender.getWikiId();
+        }
+
         return new ReceivedSurveyDto(
                 survey.getId(),
                 survey.getRelation(),
                 survey.getPeriod(),
-                survey.getSenderName()
+                survey.getSenderName(),
+                senderWikiId
         );
     }
 
