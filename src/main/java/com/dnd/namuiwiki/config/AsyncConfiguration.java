ddec1c770ee6651e307d8a3f5c766b3312a806d7
eun-seong <eunseong.dev@gmail.com>
2024-03-21 20:52:50 +0900

    refactor: 비동기 configuration 설정(#92)

---
+++ src/main/java/com/dnd/namuiwiki/config/AsyncConfiguration.java
@@ -0,0 +1,9 @@
+package com.dnd.namuiwiki.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class AsyncConfiguration {
+}
