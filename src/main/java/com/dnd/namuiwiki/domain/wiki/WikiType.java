a52cb4f2e91cdb75e410aa609893dc1276a709ff
eun-seong <eunseong.dev@gmail.com>
2024-06-13 00:59:51 +0900

    feat: WikiType enum 추가(#156)

---
+++ src/main/java/com/dnd/namuiwiki/domain/wiki/WikiType.java
@@ -0,0 +1,22 @@
+package com.dnd.namuiwiki.domain.wiki;
+
+public enum WikiType {
+    NAMUI("남의위키", "다른 사람이 보는 내 모습은 어떨까요?"),
+    RAMANCE("연애위키", "연애할 때 나는 어떤 사람인가요?");
+
+    private WikiType(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    private final String title;
+    private final String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
